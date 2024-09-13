47055a225d62c78f86ee109685d9ee84ea2f41cb
gixxerfa <118430879+gixxerfa@users.noreply.github.com>
2024-09-13 12:56:01 +0200

    pp

---
+++ Assets/programing/Elias/start_phone.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class start_phone : MonoBehaviour
+{
+    public Button start,options, exit;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

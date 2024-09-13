f756b18ecd0abfaef71e2583fad7183e1f4b9c43
gixxerfa <118430879+gixxerfa@users.noreply.github.com>
2024-09-13 13:32:54 +0200

    pp

--- Assets/programing/Elias/start_phone.cs
+++ Assets/programing/Elias/start_phone.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class start_phone : MonoBehaviour
 {
     public Button start,options, exit;
-
+   
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void newlevelbutton(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
     }
 }

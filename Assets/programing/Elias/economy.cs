bc76cb126f178728667c086ce10ddc7b930cad0d
gixxerfa <118430879+gixxerfa@users.noreply.github.com>
2024-11-13 09:14:23 +0100

    pp

--- Assets/programing/Elias/economy.cs
+++ Assets/programing/Elias/economy.cs
@@ -1,28 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class economy : MonoBehaviour
 {
-    public float money=200;
+    public float money;
+    public int dayactionnumber;
+    public int trueDayactionnumber;
+    public int inflationvalue;
+
 
     public void productcost()
     {
-        if( tag == ("_Carrot"))
+       if( tag == ("_Carrot"))
         {
-            money = money - 31;
+            money = money - 160*inflationvalue;
         }
     }
     public void infaltion()
     {
-       //if(dayaction = 3)
+        if(trueDayactionnumber == 3)
         {
-            float randomNumber = Random.Range(0, 4);
+            inflationvalue = Random.Range(1, 3);
         }
+
+    }
+    private void Start()
+    {
+        money = 20000;
     }
     public void loan()
     {
-
+        money = money * -1.1f;
     }
+    public void getmoney()
+    {
+
+        money = money + 2000;//*inflationvalue;// add inflation number prob int 
+        Debug.Log("moner");
+    }
+    public void Dayaction()
+    {
+        dayactionnumber += 1;
+        
+        if( dayactionnumber == 3 )
+        {
+            trueDayactionnumber += 1;
+        }
+        if( trueDayactionnumber == 3 )
+        {
+            //switch to diffrent day for of main room!!!
+        }
+        
+    }
+    
 
 }

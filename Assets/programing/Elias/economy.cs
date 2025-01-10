fc183148eae42201d571f85980f360f6497e9c39
gixxerfa <118430879+gixxerfa@users.noreply.github.com>
2025-01-10 08:47:38 +0100

    pp

--- Assets/programing/Elias/economy.cs
+++ Assets/programing/Elias/economy.cs
@@ -1,6 +1,7 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,14 +11,15 @@ public class economy : MonoBehaviour
     public int dayactionnumber;
     public int trueDayactionnumber;
     public int inflationvalue;
-    public int foodvalue;
-    public bool LOANING;
+    public int foodvalue, loancounter;
+    public bool SL,ML,LL,SMS, LOANING;
     public float sl, ml, ll, sms;
 
 
     public void productcost()
     {
        //if( tag == ("_Carrot"))
+
         
             money = money - 160*inflationvalue;
         
@@ -31,50 +33,50 @@ public class economy : MonoBehaviour
     }
     private void Start()
     {
+        loancounter = 3;
         inflationvalue = 1;
         money = 0;
     }
     
     public void small_loan()
-    {
-        
-        
-        if (LOANING == true)
-        {
-            money = money - sl * inflationvalue;
-        }
-
+    { 
+        SL = true;   
+      money = money + sl * inflationvalue;
         //money = money * -1.1f;
         // add to daycounter and make it dormant 
     }
     public void medium_loan()
     {
-
+      ML = true;
         //money = money * -1.1f;
         // add to daycounter and make it dormant 
 
     }
     public void large_loan()
     {
-
+        LL = true;
         //money = money * -1.1f;
         // add to daycounter and make it dormant 
     }
     public void sms_loan()
     {
-
+        SMS = true; 
     }
     public void getmoney()
     {
-
-        money = money+2000*inflationvalue;// add inflation number prob int 
-        
+        money = money+2000*inflationvalue;// add inflation number prob int        
         Debug.Log(money) ; 
     }
     private void Update()
     {
-        
-        
+        if (LOANING == true)
+        {
+            //DIABLE LOAN BUTTONS :) (currently don't have the buttons in the scene)
+            //SLbutton.enabled = false;
+            //MLbutton.enabled = false;
+            //LLbutton.enabled = false;
+            //SMSbutton.enabled = false;
+        }
         if (trueDayactionnumber == 3)
         {
             inflation();
@@ -88,30 +90,60 @@ public class economy : MonoBehaviour
         if (dayactionnumber == 3)
         {
             trueDayactionnumber += 1;
+            dayactionnumber = 0;    
         }
         if (trueDayactionnumber == 3)
         {
-            small_loan();
-            medium_loan();
-            large_loan();
-            sms_loan(); 
+            
+            
+            if(SL == true)
+            {
+                money = money * 0.98f;
+                loancounter = loancounter - 1;
+            }
+            if(loancounter == 0)
+            {
+                SL = false;
+            }
+            if (ML == true)
+            {
+                money = money * 0.95f;
+
+            }
+            if (loancounter == 0)
+            {
+                ML = false;
+            }
+            if (LL == true)
+            {
+                money = money * 0.92f;
+            }
+            if (loancounter == 0)
+            {
+                LL = false;
+            }
+            if (SMS == true)
+            {
+                money = money * 0.89f;
+            }
+            if (loancounter == 0)
+            {
+                SMS = false;
+            }
             //switch to diffrent day for of main room!!!
+            trueDayactionnumber = 0;    
         }
 
         
 
     }
-    public void hospital()
+    public void test()
     {
-        if(foodvalue==0)
-        {
-            //GetComponent<Animator>().setTrigger("hospital");
-            //add camera switch to hosptial scene!!!!!
-        }
+       
     }
-    public void slb()
+    public void test_2()
     {
-        LOANING = true;
-        money = money + 4000 * inflationvalue;
+
     }
+    
 }
--- Assets/programing/Elias/switch_camera.cs
+++ Assets/programing/Elias/switch_camera.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class switch_camera : MonoBehaviour
+public class switch_camera : economy
 {
     public GameObject Camera1;
     public GameObject Camera2;
@@ -14,7 +15,7 @@ public class switch_camera : MonoBehaviour
     public GameObject Camera8;
     public int Manager;
     public GameObject doggy;
-    public int food;
+    public int food, stupidcameranumber;
     
     
     public void ChangeCamera()
@@ -36,11 +37,32 @@ public class switch_camera : MonoBehaviour
             Manager = 0;
         }
     }
+    public void Start()
+    {
+        food = 10;
+        stupidcameranumber = 0;
+    }
     public void Update()
     {
-        if(food == 0)
+        if (food == 0)
         {
             Cam_4();
+        }
+        if (stupidcameranumber > 1)
+        {
+            Debug.Log("camera_dog");
+            //Cam_6();
+        }
+        
+        if (trueDayactionnumber == 3)
+        {
+            
+            if(stupidcameranumber == 0)
+            {
+                print("cam5");
+                //Cam_5();
+            }
+            stupidcameranumber = +1;
         }
     }
 
@@ -50,18 +72,27 @@ public class switch_camera : MonoBehaviour
         Camera1.SetActive(true);
         Camera2.SetActive(false);
         Camera3.SetActive(false);
+        Camera4.SetActive(false);
+        Camera5.SetActive(false);
+        Camera6.SetActive(false);
     }
     public void Cam_2()
     {
         Camera1.SetActive(false);
         Camera2.SetActive(true);
         Camera3.SetActive(false);
+        Camera4.SetActive(false);
+        Camera5.SetActive(false);
+        Camera6.SetActive(false);
     }
    public void Cam_3()
     {
         Camera1.SetActive(false);
         Camera2.SetActive(false);
         Camera3.SetActive(true);
+        Camera4.SetActive(false);
+        Camera5.SetActive(false);
+        Camera6.SetActive(false);
     }
     public void Cam_4()
     {
@@ -69,11 +100,46 @@ public class switch_camera : MonoBehaviour
         Camera2.SetActive(false);
         Camera3.SetActive(false);
         Camera4.SetActive(true);
+        Camera5.SetActive(false);
+        Camera6.SetActive(false);
+    }
+    public void Cam_5()
+    {
+        Camera1.SetActive(false);
+        Camera2.SetActive(false);
+        Camera3.SetActive(false);
+        Camera4.SetActive(false);
+        Camera5.SetActive(true);
+        Camera6.SetActive(false);
+    }
+    public void Cam_6()
+    {
+        Camera1.SetActive(false);
+        Camera2.SetActive(false);
+        Camera3.SetActive(false);
+        Camera4.SetActive(false);
+        Camera5.SetActive(false);
+        Camera6.SetActive(true);
     }
 
     public void dog()
     {
+        
+
         //Camera1.transform.position =doggy.transform.position;
         //GetComponent<Animator>().SetTrigger("Change");
+    }
+    public void dog_2()
+    {
+        //if (trueDayactionnumber == 3)
+        {
+
+            if (stupidcameranumber == 0)
+            {
+                print("cam5");
+                //Cam_5();
+            }
+            stupidcameranumber = stupidcameranumber +1;
+        }
     }
 }

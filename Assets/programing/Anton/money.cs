20f3d7614233b58b06da389ef1420d17765decb6
batant999 <anton.asplund06@gmail.com>
2025-01-17 10:45:56 +0100

    skdada

---
+++ Assets/programing/Anton/money.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class money : MonoBehaviour
+{
+    public TextMeshProUGUI moneyText;  // Reference to the TextMeshProUGUI component
+    private int playerMoney = 1000;    // Example starting money
+
+
+
+    private void Update()
+    {
+        moneyText.text = playerMoney.ToString();
+    }
+
+    // Example method to modify player's money
+    public void AddMoney(int amount)
+    {
+        
+        playerMoney += 200;
+    }
+
+    public void SubtractMoney(int amount)
+    {   // temporary fixes :)
+        playerMoney -= amount;
+    }
+}

b3b458f513e7f3791ba02fb4be398b1577d26b0e
batant999 <anton.asplund06@gmail.com>
2025-01-31 09:55:21 +0100

    skibidi_rizz

---
+++ Assets/programing/Anton/LoanManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoanManager : MonoBehaviour
+{
+    int LoanAmount, interestamount;
+    
+ 
+    void applyintrestrate()
+    {
+        
+    }
+
+    void TakeLoan(int amount)
+    {
+
+    }
+}

8b301e931270c57bd9165c8eee0fd8139c20bd3a
gixxerfa <118430879+gixxerfa@users.noreply.github.com>
2025-02-21 09:23:32 +0100

    pp

---
+++ Assets/programing/Elias/buttons_details_hover.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class buttons_details_hover : MonoBehaviour
+{
+    public static buttons_details_hover _instance;
+    public TextMeshProUGUI textComponent;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+    void Start()
+    {
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Input.mousePosition;
+    }
+    public void SetAndshowtooltip(string message)
+    {
+        gameObject.SetActive(true);
+        textComponent.text = message;
+    }
+    public void hidetooltip()
+    {
+        gameObject.SetActive(false);
+        textComponent.text = string.Empty;
+    }
+}
---
+++ Assets/programing/Elias/tooltip.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tooltip : MonoBehaviour
+{
+    public string message;
+
+    public void onmouseenter()
+    {
+
+    }
+    public void onmousexit() 
+    {
+    
+    
+    }
+}

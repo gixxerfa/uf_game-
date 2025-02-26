721731a7dac7049d842f6875953f1733f69938ff
batant999 <anton.asplund06@gmail.com>
2025-02-26 10:49:32 +0100

    day_move

--- Assets/programing/Anton/day_mover.cs
+++ Assets/programing/Anton/day_mover.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class day_mover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    int daynumber, dayaction;
+    // improve
+    void day_change()
     {
-        
+        if (daynumber != 3)
+        {   dayaction += 1;
+            print(dayaction);
+            print(daynumber);
+        }
+        else if (dayaction == 3)
+        {   dayaction = 0;
+            daynumber += 1;
+            print(dayaction);
+            print(daynumber);
+        }
+        else if (daynumber == 54)
+        {   
+            endgame();
+        }
+    }
+    // improve
+    void endgame()
+    {
+        // make a way to end the game
     }
 
-    // Update is called once per frame
-    void Update()
+    //improve
+    void event_managment(int event_type)
     {
-        
+        if (event_type == 1)
+        {
+            // make events happen
+        }
+        else
+        {
+            // events that can occur at the end of the day
+        }
     }
+
 }

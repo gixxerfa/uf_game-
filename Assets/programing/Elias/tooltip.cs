f2ba0f87a7d99c4f294f262910f1cee0a4002a26
gixxerfa <118430879+gixxerfa@users.noreply.github.com>
2025-02-26 10:09:34 +0100

    pp

--- Assets/programing/Elias/tooltip.cs
+++ Assets/programing/Elias/tooltip.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class tooltip : MonoBehaviour
+public class tooltip : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
 {
     public string message;
 
-    public void onmouseenter()
+    
+public void OnPointerEnter(PointerEventData eventData)
     {
+        
         buttons_details_hover._instance.SetAndshowtooltip(message);
     }
-    public void onmousexit() 
+
+    public void OnPointerExit(PointerEventData eventData)
     {
-    buttons_details_hover._instance.hidetooltip();
-    
+        
+        buttons_details_hover._instance.hidetooltip();
     }
 }
+   
+
